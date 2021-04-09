428134116c1392c794aa58b89e6d30e8adb96d18
Jackie Ye <jackieye27@gmail.com>
2021-04-08 17:01:05 -0700

    initialization of project

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import Clarifai from 'clarifai';
+import config from '../config.js';
+
+
+const key = config.key
+const app  = new Clarifai.App({
+    apiKey: key
+});
+
+export const handleApiCall = (req, res) => {
+    app.models.predict({
+        id: "a403429f2ddf4b49b307e318f00e528b",
+        version: "34ce21a40cc24b6b96ffee54aabff139",
+    }, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+export const image = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+import express from 'express'
+import bcrypt from 'bcryptjs';
+import cors from 'cors';
+import knex from 'knex';
+import handleRegister from './controllers/register.js'
+import handleSignin from './controllers/signin.js'
+import profile from './controllers/profile.js'
+import {image, handleApiCall} from './controllers/image.js'
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'jackieye',
+      password : '',
+      database : 'face-rec'
+    }
+  });
+
+  db.select('*').from('users').then(data => {
+    //   console.log(data);
+  });
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin: true, credentials: true}));
+
+//signin route check email and password
+app.post('/signin', (req, res) => {handleSignin(req, res, db, bcrypt)});
+
+// register
+app.post('/register', (req, res) => {handleRegister(req, res, db, bcrypt)});
+
+// profile
+app.get('/profile/:id', (req, res) => {profile(req, res, db)});
+
+// image entries count
+app.put('/image', (req, res) => {image(req, res, db)});
+
+//api post 
+app.post('/imageurl', (req, res) => {handleApiCall(req, res)});
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`App is listening to port ${port}`);
+})
+
+
+
+/*
+What our API will look like?
+
+1. Sign in ruest --> POST = success/fail
+2. Register --> POST = user
+3. Profile --> GET = user 
+4. Image --> PUT = updated count for user
+
+
+*/
