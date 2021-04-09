d97929a7d8d59825e7230cc408585db600e1015c
Jackie Ye <jackieye27@gmail.com>
2021-04-08 18:26:22 -0700

    changed ss1 property

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ss1: true 
+      ss1: {
+          rejectUnauthorized: false
+      } 
     }
   });
 
